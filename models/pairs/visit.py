7ef6b4430a7d8ce29470e8bde586ff2213c4fa32
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-19 20:43:23 +0300

    create_visit_view

--- models/pairs/visit.py
+++ models/pairs/visit.py
@@ -6,6 +6,8 @@ from models.model import BaseModel
 class Visit(BaseModel):
     __tablename__ = 'visit'
 
+    CREATE = 'Create.Visit'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     when = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey('user.id'), comment='User')
--- pairs/api/visits.py
+++ pairs/api/visits.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_session
 from models import Visit
 from pairs.schemas.visitis import CreateVisit, DefaultVisit
+from permissions.controllers.permissions import is_action_allowed
+from users.authentications.authenticators import is_user_authenticated
+from users.schemas.users import DefaultUser
 
 visits_router = APIRouter(prefix="/visits")
 
 
 @visits_router.post('', tags=['visits'], response_model=DefaultVisit)
-async def create_visit_view(create_visit: CreateVisit, session: AsyncSession = Depends(get_session)):
-    return await Visit.create(session, **create_visit.dict())
+async def create_visit_view(
+        create_visit: CreateVisit,
+        session: AsyncSession = Depends(get_session),
+        user: DefaultUser = Depends(is_user_authenticated)
+):
+    if await is_action_allowed([Visit.CREATE], session, user):
+        return await Visit.create(session, **create_visit.dict())
+
+    return Response(status_code=status.HTTP_403_FORBIDDEN)
