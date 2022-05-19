983f561cd99a970010fffc5ad95ac8c95cf59e1d
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-19 23:21:59 +0300

    Update pairs.py

--- pairs/schemas/pairs.py
+++ pairs/schemas/pairs.py
@@ -6,13 +6,13 @@ from pydantic import BaseModel, Field
 
 class CreatePair(BaseModel):
     name: str
+    room: Optional[str]
+    start_at: Optional[time] = Field(alias='startAt')
+    end_at: Optional[time] = Field(alias='endAt')
 
 
 class DefaultPair(CreatePair):
     id: int
-    room: Optional[str]
-    start_at: Optional[time] = Field(alias='startAt')
-    end_at: Optional[time] = Field(alias='endAt')
 
     class Config:
         orm_mode = True
