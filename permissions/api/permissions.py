351211cf240a0e8684836826201dedc3d7f1c52e
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-22 02:01:40 +0300

    get_my_permissions

--- permissions/api/permissions.py
+++ permissions/api/permissions.py
@@ -5,7 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_session
 from models import Permission
+from permissions.controllers.permissions import get_my_permissions
 from permissions.schema.permissions import DefaultPermission
+from users.authentications.authenticators import is_user_authenticated
+from users.schemas.users import DefaultUser
 
 permissions_router = APIRouter(prefix="/permissions")
 
@@ -13,3 +16,11 @@ permissions_router = APIRouter(prefix="/permissions")
 @permissions_router.get('', tags=['permissions'], response_model=List[DefaultPermission])
 async def get_permissions_view(session: AsyncSession = Depends(get_session)):
     return await Permission.filter(session)
+
+
+@permissions_router.get('/me', tags=['permissions'], response_model=List[DefaultPermission])
+async def get_my_permissions_view(
+        session: AsyncSession = Depends(get_session),
+        user: DefaultUser = Depends(is_user_authenticated)
+):
+    return await get_my_permissions(session, user)
--- permissions/controllers/permissions.py
+++ permissions/controllers/permissions.py
@@ -7,7 +7,7 @@ from models import Role, UserRole, Permission, RolePermission
 from users.schemas.users import DefaultUser
 
 
-async def is_action_allowed(scopes: List[str], session: AsyncSession, user: DefaultUser) -> bool:
+async def get_my_permissions(session: AsyncSession, user: DefaultUser) -> List[Permission]:
     roles_query = select(Role) \
         .join(UserRole) \
         .filter(UserRole.user_id == user.id)
@@ -16,6 +16,10 @@ async def is_action_allowed(scopes: List[str], session: AsyncSession, user: Defa
     permissions_query = select(Permission) \
         .join(RolePermission) \
         .filter(RolePermission.role_id.in_(role.id for role in roles))
-    permissions: List[Permission] = (await session.execute(permissions_query)).scalars().all()
+    return (await session.execute(permissions_query)).scalars().all()
+
+
+async def is_action_allowed(scopes: List[str], session: AsyncSession, user: DefaultUser) -> bool:
+    permissions = await get_my_permissions(session, user)
 
     return any((permission.scope in scopes) for permission in permissions)
