0c77f275c50a935b694b363bc41f15ddee18e10d
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-22 23:54:50 +0300

    get_learner_pairs

--- pairs/controllers/pairs.py
+++ pairs/controllers/pairs.py
@@ -6,6 +6,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models import UserPair, Pair
 
 
-async def get_self_pairs(session: AsyncSession, user_id: int) -> List[Pair]:
+async def get_learner_pairs(session: AsyncSession, user_id: int) -> List[Pair]:
     query = select(Pair).join(UserPair).filter(UserPair.user_id == user_id)
     return (await session.execute(query)).scalars().all()
