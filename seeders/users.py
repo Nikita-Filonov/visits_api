618da5232c216ff0ebe8953642b136e01be9a47f
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-26 21:23:21 +0300

    Seeders

--- seeders/users.py
+++ seeders/users.py
@@ -5,20 +5,30 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from database import engine
-from models import User, Token
+from models import User, Token, UserRole
 from users.schemas.users import CreateUser
 
 BASE_PATH = './seeders/dumps'
 users = json.loads(open(f'{BASE_PATH}/users.json').read())
+user_roles = json.loads(open(f'{BASE_PATH}/user_roles.json').read())
 
 
 async def seed_users():
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
-        for user in users:
+        for user, user_role in zip(users, user_roles):
+            u = await User.get(session, email=user['email'])
+            await Token.delete(session, user_id=u.id)
+            await u.delete(session, id=u.id)
+
+            is_user_exists = await User.get(session, email=user['email'])
+            if is_user_exists:
+                return
+
             user_payload = CreateUser(**user).dict()
             user = await User.create(session, **user_payload, is_active=True, is_email_confirmed=True)
             await Token.create(session, user)
+            await UserRole.create(session, user_id=user.id, role_id=user_role['role_id'])
 
 
 if __name__ == '__main__':
