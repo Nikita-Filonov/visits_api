0fb3b4db041790b8b9610f3bea8de9ea7d9fa02c
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-28 19:34:26 +0300

    Update users.py

--- users/api/users.py
+++ users/api/users.py
@@ -16,6 +16,10 @@ users_router = APIRouter(prefix="/user")
 
 @users_router.get("/me/", tags=['user'], response_model=DefaultUser)
 async def read_users_me(current_user: DefaultUser = Depends(is_user_authenticated)):
+    """
+    :param current_user: Объект ``DefaultUser`` текущий пользователь
+    :return: Возвращает объект ``DefaultUser``
+    """
     return current_user
 
 
@@ -27,6 +31,15 @@ async def get_users_query(
         session: AsyncSession = Depends(get_session),
         user: DefaultUser = Depends(is_user_authenticated),
 ):
+    """
+    :param username: String параметр ``username``, который передается в query params
+    :param email: String параметр ``email``, который передается в query params
+    :param limit: Integer параметр запроса, который передается в query params.
+    Отвечает за количество возвращаемый пользователей
+    :param session: см. README.md ``Объекты``
+    :param user: см. README.md ``Объекты``
+    :return: Возвращает список объектов ``DefaultUser``
+    """
     if not await is_action_allowed([User.VIEW], session, user):
         return Response(status_code=status.HTTP_403_FORBIDDEN)
 
@@ -35,6 +48,11 @@ async def get_users_query(
 
 @users_router.post("/", tags=['user'], response_model=DefaultUser)
 async def create_user(user: CreateUser, session: AsyncSession = Depends(get_session)):
+    """
+    :param user: см. README.md ``Объекты``
+    :param session: см. README.md ``Объекты``
+    :return: Возвращает объект ``DefaultUser``
+    """
     user = await User.create(session, **user.dict())
     await send_user_confirm_email(session, user)
     return user
@@ -42,6 +60,13 @@ async def create_user(user: CreateUser, session: AsyncSession = Depends(get_sess
 
 @users_router.post("/send-confirm-email/", tags=['user'])
 async def send_confirm_email(user: ConfirmUser, session: AsyncSession = Depends(get_session)):
+    """
+    :param user: Объект ``ConfirmUser``
+    :param session: см. README.md ``Объекты``
+    :return: Возвращает статус код 202, если письмо для подтверждения почты
+    было учпешно отправлено. Код 403, если пользователя с почтой ``ConfirmUser.email``
+    не существует или он уже активирован
+    """
     user = await User.get(session, email=user.email)
     if user is None:
         return Response(status_code=status.HTTP_403_FORBIDDEN)
@@ -55,6 +80,13 @@ async def send_confirm_email(user: ConfirmUser, session: AsyncSession = Depends(
 
 @users_router.post("/confirm-email/", tags=['user'])
 async def confirm_email(user_confirm: ConfirmUser, session: AsyncSession = Depends(get_session)):
+    """
+    :param user_confirm: Объект ``ConfirmUser``
+    :param session: см. README.md ``Объекты``
+    :return: Возвращает токен если активация пользователя прошла успешно.
+    Если код подтвержения не верный или пользователь уже активирован, то
+    возвращает 403 код
+    """
     user = await User.get(session, email=user_confirm.email)
     if user is None or user.is_email_confirmed:
         return Response(status_code=status.HTTP_403_FORBIDDEN)
