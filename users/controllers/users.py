791e58a27e67e7510f1ee39d99c673fc1495a832
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-29 11:56:20 +0300

    Update users.py

--- users/controllers/users.py
+++ users/controllers/users.py
@@ -1,24 +1,9 @@
 from typing import List
 
-from fastapi import status, HTTPException
 from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import User
-from users.schemas.users import UserNotFound
-
-
-async def safely_get_user(session: AsyncSession, **user_fields) -> User:
-    safe_values = {key: value for key, value in user_fields.items() if value is not None}
-
-    user = await User.get(session, **safe_values)
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=UserNotFound(args=list(safe_values.values())).dict()
-        )
-
-    return user
 
 
 async def safely_get_users(session: AsyncSession, limit: int, username: str = None, email: str = None) -> List[User]:
